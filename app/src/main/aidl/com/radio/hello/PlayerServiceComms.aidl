6513eec9db753d102b19c8fb74eab030017bbcc1
sivasankarankb <kbsiva7@gmail.com>
2020-03-19 10:30:10 +0530

    Implemented player service binding. Improved UI.

---
+++ app/src/main/aidl/com/radio/hello/PlayerServiceComms.aidl
@@ -0,0 +1,8 @@
+// PlayerServiceComms.aidl
+package com.radio.hello;
+
+interface PlayerServiceComms {
+    void startPlayer();
+    void pausePlayer();
+    void stopPlayerService();
+}
--- app/src/main/java/com/radio/hello/MainActivity.java
+++ app/src/main/java/com/radio/hello/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -16,16 +18,25 @@ public class MainActivity extends AppCompatActivity {
     ImageButton playPauseBtn;
     Boolean isPlaying = true;
 
+    PlayerServiceComms comms;
+    PlayerServiceConnection connection;
+
     private class PlayerServiceConnection implements ServiceConnection{
 
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-
+            comms = PlayerServiceComms.Stub.asInterface(service);
+            try {
+                comms.startPlayer();
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
-
+            comms = null;
+            Log.d("MainActivity","PlayerService has been disconnected.");
         }
     }
 
@@ -34,11 +45,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent playerServiceIntent = new Intent(MainActivity.this, PlayerService.class);
         playPauseBtn = findViewById(R.id.playPauseBtn);
+        connection = new PlayerServiceConnection();
 
-        final Intent intent = new Intent(MainActivity.this, PlayerService.class);
-        startService(intent);
-        bindService(intent, new PlayerServiceConnection(), 0);
+        bindService(playerServiceIntent, connection, BIND_AUTO_CREATE);
         showToast();
 
         playPauseBtn.setOnClickListener(new View.OnClickListener() {
@@ -48,13 +59,21 @@ public class MainActivity extends AppCompatActivity {
                     playPauseBtn.setImageResource(R.drawable.play);
                     isPlaying = false;
 //                   pauseMusic();
-                    stopService(intent);
+                    try {
+                        comms.pausePlayer();
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
 
                 } else {
                     playPauseBtn.setImageResource(R.drawable.pause);
                     isPlaying = true;
 //                   playMusic();
-                    startService(intent);
+                    try {
+                        comms.startPlayer();
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
                     showToast();
 
                 }
@@ -67,4 +86,9 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Buffering...", Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(connection);
+    }
 }
